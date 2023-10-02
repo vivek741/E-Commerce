cba888417daf686a97a417099266cd43ae99df2b
vivek@741 <vivek.choudhary.741@gmail.com>
2023-10-03 01:32:36 +0530

    Secound commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Checkout from "./Checkout";
+import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path="./Checkout ">
+            <Header />
+            <Checkout />
+          </Route>
+          
+          <Route path="/">
+            <Header />
+            <Home /> {/*Home route should come at the end of all roures */}
+          </Route>  
+
+        </Switch>
+      </Router>
+
       <Header />
       <Home />
     </div>
